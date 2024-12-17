678778ecede49d2b89288551255ca2cbe18a0709
gurayyalmaz <guray.yl93@gmail.com>
2024-12-17 12:09:19 +0100

    Initial Commit

---
+++ Die_worte/Die_worteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Die_worteApp.swift
+//  Die_worte
+//
+//  Created by Gyuray Yalmaz on 17.12.24.
+//
+
+import SwiftUI
+
+@main
+struct Die_worteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
